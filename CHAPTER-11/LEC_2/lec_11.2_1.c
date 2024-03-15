2ed7bcf39b89fe4a95da616106bca04fa0981a04
mohit-ramani-009 <mohitramani404@gmail.com>
2024-03-15 13:21:52 +0530

    my work

---
+++ CHAPTER-11/LEC_2/lec_11.2_1.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#define m printf
+#define d scanf
+
+void ReverseArray(int *arr, int size) 
+{
+    int *a = arr;
+    int *b = arr + size - 1;
+    
+    while (a < b) 
+	{
+        int c = *a;
+        *a = *b;
+        *b = c;
+        a++;
+        b--;
+    }
+}
+
+void main() 
+{
+    int size;
+    int i;
+    
+    m("Enter the array's size: ");
+    d("%d", &size);
+    
+    int array[size];
+    
+    m("Enter the array elements: \n");
+    
+    for (i = 0; i < size; i++) 
+	{
+        m("a[%d] = ", i);
+        d("%d", &array[i]);
+    }
+    
+    ReverseArray(array, size);
+    
+    m("Reversed array elements: \n");
+    for (i = 0; i < size; i++) 
+	{
+        m("%d ", array[i]); 
+    }
+}
+
+
